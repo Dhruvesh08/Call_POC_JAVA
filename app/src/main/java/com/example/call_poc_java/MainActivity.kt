79ce4f00e739b4a281123f06b57287e4367a0787
Dhruvesh <dhruveshbaria@gmail.com>
2022-04-10 19:29:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/call_poc_java/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.call_poc_java
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.call_poc_java.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
